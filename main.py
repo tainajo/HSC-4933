4043e6ae5a339099d3ef5aec587decbe6edd5ec1
tainajo <tainajoseph@usf.edu>
2026-08-26 10:46:23 -0400

    Add main.py via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#####################################
+# Taina Joseph #tainajoseph@usf.edu #
+#-----------------------------------#
+
+""" we want this script to print the string Hello, World!"""
+
+print("Hello World!")
+
+a = "Hello, World!"
+print(a)
