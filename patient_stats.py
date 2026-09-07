064000bf952d797a71bd9e44411d071d940b3ce6
tainajo <tainajoseph@usf.edu>
2026-09-06 23:24:18 -0400

    Add files via upload

---
+++ patient_stats.py
@@ -0,0 +1,13 @@
+"Enter patient id number "
+patient_id = int(input("Enter patient id number: "))
+if patient_id == 148:
+    print("J. Alvarez")
+    print("Heart Rate Values: 72, 75, 78 ")
+
+if patient_id == 149:
+    print("M. Chen")
+    print("Heart Rate Values: 80, 82 ")
+
+if patient_id == 150:
+    print("R.Okafor")
+    print("Heart Rate Values: 65, 68, 70, 66  ")
