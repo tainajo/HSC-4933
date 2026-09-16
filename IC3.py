0ad392f359672d8517005d04df345ce52035481b
tainajo <tainajoseph@usf.edu>
2026-09-16 10:40:50 -0400

    Add files via upload

---
+++ IC3.py
@@ -0,0 +1,28 @@
+import requests
+YEAR = 2020
+DATASET = "dec/pl"
+
+URL = f"https://api.census.gov/data/{YEAR}/{DATASET}"
+API_KEY = "ba387dd3093f1deccf93db1cc6567bbce97d3ab1"
+
+param = {
+"get" : "NAME,P1_001N",
+"for" : "state:*",
+"key" : API_KEY,
+}
+
+response = requests.get(URL, params=param)
+
+if response.status_code != 200:
+    print(f"Request failed ({response.status_code})")
+    print(response.text)
+    raise SystemExit (1)
+
+response.raise_for_status()
+
+data = response.json()
+
+print(f"Got {len(data) -1} rows back.")
+
+for i in data:
+    print(i)
